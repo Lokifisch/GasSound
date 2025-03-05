443ff93a3a250a6c74e8cf302f0fc3fa21ef48f3
Lokifisch <lokifischyt@gmail.com>
2025-03-05 11:40:59 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 music.play(music.createSoundExpression(WaveShape.Sawtooth, 1, 496, 255, 255, 9999, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
 music.play(music.createSoundExpression(WaveShape.Sawtooth, 496, 917, 255, 255, 9999, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
 while (true) {
-    music.play(music.createSoundExpression(WaveShape.Sawtooth, 917, 917, 255, 255, 9999, SoundExpressionEffect.Tremolo, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
+    music.play(music.createSoundExpression(WaveShape.Sawtooth, 917, 917, 255, 255, 9999, SoundExpressionEffect.Vibrato, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
 }
